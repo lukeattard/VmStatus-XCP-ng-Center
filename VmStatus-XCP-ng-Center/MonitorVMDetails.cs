97bb71e2e9d149647bdd35b29482778a87cb7d14
Luke Attard <luke@rapiddata.com.au>
2020-05-07 22:58:42 +1000

    Add project files.

---
+++ VmStatus-XCP-ng-Center/MonitorVMDetails.cs
@@ -0,0 +1,139 @@
+﻿using Renci.SshNet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using VmStatus_XCP_ng_Center.Properties;
+
+namespace VmStatus_XCP_ng_Center
+{
+  public class MonitorVMDetails
+  {
+    internal string Status { get; set; }
+    internal string Hostname { get; set; }
+    internal string Username { get; set; }
+    internal string Password { get; set; }
+    internal int Port { get; set; }
+    internal string VmUUID { get; set; }
+    internal string ErrorMessage { get; set;  }
+    private SshClient SshVmClient;
+
+    //TODO depending on how many status we end up with, maybe make into a static list.
+    //TODO add language file and replace entries making program multi language
+    private static string connected = "connected";
+    private static string disconnected = "disconnected";
+    private static string error = "error";
+    private static string nr = "nr";
+    private static string completed = "completed";
+    private static string unknown = "unknown";
+    
+    public MonitorVMDetails()
+    {
+      //TODO - this is the long way and not necessary right now however will be required for list to be added later, new class of MonitorVMList will create this class for each VM, at that time the construct will be cahnged to take arguments. 
+
+      Hostname = Resources.hostname;
+      Username = Resources.username;
+      Password = Resources.password;
+      Port = Int16.Parse(Resources.port);
+      VmUUID = Resources.vmUUID;
+      Status = unknown;
+      ConnectVM();
+    }
+
+    internal string ConnectVM()
+    {
+      if(Status.Equals(connected))
+      {
+        return (nr);
+      }
+
+      try
+      {
+        SshVmClient = new SshClient(Hostname, Port, Username, Password);
+        SshVmClient.Connect();
+      }
+      catch (Exception e)
+      {
+        Status = error;
+        ErrorMessage = e.Message;
+      }
+
+      Status = connected;
+      return (connected);
+    }
+
+    internal string DisconnectVM()
+    {
+      if ( SshVmClient == null || Status.Equals(disconnected))
+      {
+        return (nr);
+      }
+
+      try
+      {
+        SshVmClient.Disconnect();
+      }
+      catch (Exception ex)
+      {
+        Status = error;
+        ErrorMessage = ex.Message;
+        return (Status);
+      }
+
+      Status = disconnected;
+      return (disconnected);
+    }
+    internal string UpdateStatus()
+    {
+      //TODO add command of whoami - confirm response = connect no response = disconnected.
+      return (Status);
+    }
+
+    internal string StartVM(MonitorVMDetails currentVM)
+    {
+      if((currentVM.Status.Equals(unknown) || !currentVM.Status.Equals(connected)) && currentVM.ConnectVM().Equals(error))
+      {
+        currentVM.Status = error;
+        return (error);
+      }
+      try
+      {
+        currentVM.SshVmClient.RunCommand($"sudo xe vm-start vm={ currentVM.VmUUID } ");
+      }
+      catch (Exception ex)
+      {
+        ErrorMessage = ex.Message;
+        currentVM.Status = error;
+        return (error);
+      }
+      return (completed);
+    }
+    internal string ShutdownVM(MonitorVMDetails currentVM)
+    {
+      if(currentVM.Status.Equals(error) && !currentVM.ConnectVM().Equals(connected))
+      {
+        return (error);
+      }
+      else if(!currentVM.Status.Equals(connected) && !currentVM.ConnectVM().Equals(connected))
+      {
+        return (currentVM.Status);
+      }
+      try
+      {
+        currentVM.SshVmClient.RunCommand($"sudo xe vm-shutdown vm={ Resources.vmUUID } ");
+      }
+      catch (Exception ex)
+      {
+        currentVM.ErrorMessage = ex.Message;
+        return(currentVM.ErrorMessage);
+      }
+      return(completed);
+    }
+
+  }
+}
---
+++ VmStatus-XCP-ng-Center/TaskbarStatus.cs
@@ -0,0 +1,49 @@
+﻿using Renci.SshNet;
+using System;
+using System.Windows.Forms;
+using VmStatus_XCP_ng_Center;
+using VmStatus_XCP_ng_Center.Properties;
+
+public class TaskbarStatus : ApplicationContext
+{
+  private NotifyIcon TrayIcon;
+  MonitorVMDetails SingleVM = new MonitorVMDetails();
+  internal string StatusMessage = "";
+
+  public TaskbarStatus()
+  {
+    // Initialize Tray Icon
+    TrayIcon = new NotifyIcon()
+    {
+      Icon = Resources.NotifyIcon,
+      ContextMenu = new ContextMenu(new MenuItem[] {
+       new MenuItem("Start", StartVM_Click),
+       new MenuItem("Shutdown VM", ShutdownVM_Click),
+       new MenuItem("Settings", SettingsVM_Click),
+       new MenuItem("Exit", OnExit_Click)
+      }),
+      Visible = true
+
+    };
+  }
+  void StartVM_Click(object sender, EventArgs e)
+  {
+    StatusMessage = SingleVM.StartVM(SingleVM);
+  }
+  void ShutdownVM_Click(object sender, EventArgs e)
+  {
+    StatusMessage = SingleVM.ShutdownVM(SingleVM);
+  }
+  void SettingsVM_Click(object sender, EventArgs e)
+  {
+    //TODO - add a form to display the current VM and have the ability to change the settings.  
+    //TODO-LT - Change to list of VM's that can be added to - store in a XML file with hashing of pass, or maybe add a SQLlight DB, LDAP, or MYSQL
+  }
+  void OnExit_Click(object sender, EventArgs e)
+  {
+    // Hide tray icon, otherwise it will remain shown until user mouses over it
+    TrayIcon.Visible = false;
+
+    Application.Exit();
+  }
+}
